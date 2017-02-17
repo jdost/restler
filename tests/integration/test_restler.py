15a5f2f26a4e0e03657e8031b44e60b4902dc0b0
Jeff <jostendorf@gmail.com>
2017-02-17 16:38:03 -0600

    Testfix - fix integration test

--- tests/integration/test_restler.py
+++ tests/integration/test_restler.py
@@ -24,7 +24,7 @@ class TestRestler(unittest.TestCase):
         defaults
         '''
         response = self.local()
-        self.assertEquals(response.url, "http://127.0.0.1:9001")
+        self.assertEquals(response.url, "http://127.0.0.1:9001/")
         self.assertEquals(response.data['method'], "GET")
         self.assertEquals(response.data['params'], {})
 
